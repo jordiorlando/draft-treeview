4085709646c14e9247e47c83f69a7dce694a939f
Jordi Orlando <jordi.orlando@gmail.com>
2016-01-06 15:21:02 -0600

    Initial commit

---
+++ draft-treeview.js
@@ -0,0 +1,68 @@
+(function() {
+  // TODO: allow treeview to be attached to any DOM element
+  // TODO: make each element in the treeview into its own element
+  var treeview = {
+    require: [
+      Draft.json || 'json'
+    ],
+
+    createTreeView: function() {
+      var treeView = document.createElement('div');
+      treeView.className = 'tree-view';
+
+      // treeView.appendChild(document.createElement('span'));
+      // treeView.firstChild.textContent = 'Document Model:';
+
+      var pre = document.createElement('pre');
+      treeView.appendChild(pre);
+
+      // Make sure this.dom is initialized
+      this.dom = this.dom || {};
+      this.dom.treeView = treeView;
+
+      treeView.addEventListener('update', function(e) {
+        console.log('tree-view event:', e.detail.element);
+        e.detail.element.updateTreeView();
+      });
+
+      return this.updateTreeView();
+    },
+
+    updateTreeView: function() {
+      var replacer = function(key, value) {
+        if (key == 'dom' || key == 'parent' || key == 'id' || key == 'type') {
+          return undefined;
+        } else if (key == 'children') {
+          var obj = {};
+          for (let element of value) {
+            obj[Draft.domID(element)] = element;
+          }
+          return obj;
+        } else {
+          return value;
+        }
+      };
+
+      var treeString = this.stringify(replacer).split('"').join('');
+      this.dom.treeView.firstChild.textContent = Draft.domID(this) + ': ' + treeString;
+
+      var longestLine = treeString.split('\n').reduce(function(a, b) {
+        return a.length > b.length ? a : b;
+      });
+      // FIXME: change 84 to a non-hardcoded value
+      this.dom.treeView.style.width = Math.min(longestLine.length + 4, 84) + 'ch';
+
+      return this.dom.treeView;
+    }
+  };
+
+  // Draft.extend(Draft.Container, treeview);
+  Draft.Container.extend(treeview);
+
+  /*var css = document.createElement('link');
+  css.setAttribute('rel', 'stylesheet');
+  css.setAttribute('type', 'text/css');
+  css.setAttribute('href', 'bower_components/draft-treeview/main.css');
+
+  document.getElementsByTagName('head')[0].appendChild(css);*/
+})();
