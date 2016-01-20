af9abdd75c6a315c2ec2c39fb7516d2350b0d27b
Jordi Orlando <jordi.orlando@gmail.com>
2016-01-19 21:12:56 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* eslint dot-location: [2, "property"] */
+
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const header = require('gulp-header');
@@ -25,31 +27,36 @@ var headerLong = [
   '*/\n'
 ].join('\n');
 
-var headerShort = '/*<%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> license*/\n';
+var headerShort = [
+  '/*<%= pkg.name %> v<%= pkg.version %>',
+  '<%= pkg.homepage %>',
+  '<%= pkg.license %> license*/\n'
+].join(' | ');
 
 
 
 gulp.task('clean', function() {
-	del.sync(['dist/*']);
+  del.sync(['dist/*']);
 });
 
 gulp.task('unify', ['clean'], function() {
   pkg.buildDate = Date();
 
   return gulp.src('draft-treeview.js')
-    .pipe(header(headerLong, { pkg: pkg }))
+    .pipe(header(headerLong, {pkg: pkg}))
     .pipe(gulp.dest('dist'))
     .pipe(size({showFiles: true, title: 'Full'}));
 });
 
 gulp.task('minify', ['unify'], function() {
   return gulp.src('dist/draft-treeview.js')
-    .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(babel({ presets: ['es2015'] }))
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(babel({presets: ['es2015']}))
       .pipe(uglify())
-      .pipe(rename({ suffix:'.min' }))
-      .pipe(size({ showFiles: true, title: 'Minified' }))
-      .pipe(size({ showFiles: true, gzip: true, title: 'Gzipped' }))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(header(headerShort, {pkg: pkg}))
+      .pipe(size({showFiles: true, title: 'Minified'}))
+      .pipe(size({showFiles: true, gzip: true, title: 'Gzipped'}))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
